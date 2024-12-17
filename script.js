b379db319eddbef0ddd0c52c0c6863f4ed04571f
Juampatagle <juampatagle@gmail.com>
2024-12-16 23:19:28 -0300

    termine de agregar animaciones y la parte de inicio

--- script.js
+++ script.js
@@ -1,8 +1,27 @@
-// Obtener los elementos de la navbar
-const hamburgerIcon = document.getElementById('hamburger-icon');
-const navbarMenu = document.querySelector('.navbar-menu');
+// Obtener todos los elementos con la clase 'animate-text'
+const elementos = document.querySelectorAll('.animate-text');
 
-// Añadir un listener para que el menú se despliegue y repliegue
-hamburgerIcon.addEventListener('click', () => {
-    navbarMenu.classList.toggle('active'); // Activar o desactivar el menú
+// Función para verificar si un elemento está en la pantalla
+const observer = new IntersectionObserver((entradas, observer) => {
+    entradas.forEach(entrada => {
+        // Si el elemento está en la pantalla, activar la animación
+        if (entrada.isIntersecting) {
+            entrada.target.classList.add('animate-in-llamativa');
+            observer.unobserve(entrada.target); // Dejar de observar el elemento una vez que ha sido animado
+        }
+    });
+}, {
+    threshold: 0.5 // Se activará cuando el 50% del elemento sea visible
 });
+
+// Comenzar a observar cada elemento
+elementos.forEach(elemento => {
+    observer.observe(elemento);
+});
+
+// Al cargar la página, activamos la animación para la Navbar
+window.addEventListener('load', () => {
+    const navbar = document.querySelector('.navbar');
+    navbar.classList.add('animate-navbar');
+});
+
